ccef423d57c110e34460e2fc064c418ca67d3873
Vincent Weevers <mail@vincentweevers.nl>
2018-09-11 15:41:00 +0200

    first test

--- test.js
+++ test.js
@@ -1,1 +1,20 @@
-// TODO
+'use strict'
+
+const test = require('tape')
+const StudentHistogram = require('.')
+
+test('basic', function (t) {
+  const h = new StudentHistogram(1, 100, 3)
+
+  t.is(h.record(10), true)
+  t.is(h.record(30), true)
+
+  t.is(h.size, 2)
+  t.is(h.mean(), 20)
+  t.is(h.stddev(), 10)
+  t.is(h.min(), 10)
+  t.is(h.max(), 30)
+  t.is(h.df(), 1)
+
+  t.end()
+})
